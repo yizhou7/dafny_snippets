5ef93eb4769cc334f6b2793ca93000753fa4302d
Yi Zhou <yizhou7@outlook.com>
2020-03-28 11:06:46 -0400

    done renaming?

--- test.cpp
+++ test.cpp
@@ -44,28 +44,24 @@ void subM(const RSAPublicKey *key, uint32_t *a)
 // m' = 3837733825 # (m'* N) % B == -1
 // R_inv = 64906 # (R * R_inv) % N == 1
 
-uint32_t c[4] = {0};
-uint32_t b[4] = {123};
-uint32_t n[4] = {72639};
-
-RSAPublicKey key {
-    .len = 1,
-    .n = n,
-    .n0inv = 3837733825,
-};
+// RSAPublicKey key {
+//     .len = 1,
+//     .n = n,
+//     .n0inv = 3837733825,
+// };
 
 /* montgomery c[] += a * b[] / R % mod */
 static void montMulAdd(const RSAPublicKey *key,
                        uint32_t* A,
                        const uint32_t x_i,
                        const uint32_t* y) {
-    uint64_t p_1 = (uint64_t)x_i * b[0] + A[0];
+    uint64_t p_1 = (uint64_t)x_i * y[0] + A[0];
     uint32_t u_i = (uint32_t)p_1 * key->n0inv;
     uint64_t p_2 = (uint64_t)u_i * key->n[0] + (uint32_t)p_1;
 
     int i;
     for (i = 1; i < key->len; ++i) {
-        p_1 = (p_1 >> 32) + (uint64_t)x_i * y[i] + c[i];
+        p_1 = (p_1 >> 32) + (uint64_t)x_i * y[i] + A[i];
         p_2 = (p_2 >> 32) + (uint64_t)u_i * key->n[i] + (uint32_t)p_1;
         A[i - 1] = (uint32_t)p_2;
     }
