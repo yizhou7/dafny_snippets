378cbb277459be15d0a019453bdbe1315f951f67
Yi Zhou <yizhou7@outlook.com>
2020-05-02 14:28:16 -0400

    adding some stats on the asms

--- rsicv_impl/get_stats.py
+++ rsicv_impl/get_stats.py
@@ -1,18 +1,24 @@
-import sys
 import re
+import glob
 
+def print_stats(file_name):
+	unique_opcodes = set()
+	totoal_ins_count = 0
+
+	with open(file_name) as f:
+		for line in f.readlines():
+			if re.match(pattern, line):
+				totoal_ins_count += 1
+				line = line.strip().split("\t")
+				unique_opcodes.add(line[0])
+
+	print("file_name: " + file_name)
+	print("totoal_ins_count: %d" % totoal_ins_count)
+	print("uique_opcode_count: %d" % len(unique_opcodes))
+	print("")
+
+s_files = sorted(glob.glob("*.s"))
 pattern = re.compile("^\t[a-z]")
 
-unique_opcodes = set()
-totoal_ins_count = 0
-
-with open(sys.argv[1]) as f:
-	for line in f.readlines():
-		if re.match(pattern, line):
-			totoal_ins_count += 1
-			line = line.strip().split("\t")
-			unique_opcodes.add(line[0])
-
-print("totoal_ins_count: %d" % totoal_ins_count)
-print("uique_opcode_count: %d" % len(unique_opcodes))
-# print(unique_opcodes)
+for s_file in s_files:
+	print_stats(s_file)
