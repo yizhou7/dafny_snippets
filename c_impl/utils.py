a126ba920b66b6f8aed1e3ddf78c8334c91ab03f
Yi Zhou <yizhou7@outlook.com>
2020-05-10 15:46:02 -0400

    help me create example

---
+++ c_impl/utils.py
@@ -0,0 +1,41 @@
+import sys
+
+R = 2 **32
+B = 2 **32
+
+def mod_inverse(a, m) : 
+    m0 = m 
+    y = 0
+    x = 1
+  
+    if (m == 1) : 
+        return 0
+    while (a > 1) : 
+        # q is quotient 
+        q = a // m 
+        t = m 
+        # m is remainder now, process 
+        # same as Euclid's algo 
+        m = a % m 
+        a = t 
+        t = y 
+        # Update x and y 
+        y = x - q * y 
+        x = t 
+    # Make x positive 
+    if (x < 0) : 
+        x = x + m0 
+  
+    return x 
+
+def compute_rr(n):
+    return R * R % n
+
+n = int(sys.argv[1], 16)
+print("n/R:", n/R)
+rr = compute_rr(n) 
+print("rr/R:", rr/R)
+
+n0inv = B - mod_inverse(n, B)
+print(hex(rr))
+print(hex(n0inv))
